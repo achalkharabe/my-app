650f9f4cf2d8703a0a9fbbdd7d8fe3bea5cd7e51
achalkharabe <achalkharabe1@gmail.com>
2023-05-18 14:28:34 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           rel="noopener noreferrer"
         >
           <h1>hellow achal kharabe</h1>
-          <h2>these is my 3rd commit</h2>
+          <h2>these is my 5th commit</h2>
           Learn React
         </a>
       </header>
